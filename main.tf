158d53ccb4a9acfb5bf5e3bd9b3debfa27561fef
Wes Higbee <kareemabdallah@hotmail.com>
2020-02-13 12:11:59 +0100

    modifying main.tf

--- main.tf
+++ main.tf
@@ -1,25 +1,4 @@
-#Define variables
-variable "image_name" {
-  description = "Image for container."
-  default = "ghost:latest"
-}
 
-variable "container_name" {
-  description = "Name of the container."
-  default = "blog"
-}
-
-variable "int_port" {
-  description = "Internal port for container."
-  default = "2368"
-}
-
-variable "ext_port" {
-  description = "external port for container."
-  default = "80"
-}
-
-# Download the latest Ghost image
 resource "docker_image" "image_id" {
   name = "${var.image_name}"
 }
@@ -33,15 +12,3 @@ resource "docker_container" "container_id" {
     external = "${var.ext_port}"
   }
 }
-
-#Output the IP Address of the Container
-output "ip_address" {
-  value =  "${docker_container.container_id.ip_address}"
-  description = "The IP for the container."
-}
-#Output the Name of the Container
-output "container_name" {
-  value = "${docker_container.container_id.name}"
-  description = "The name of the container."
-}
-
