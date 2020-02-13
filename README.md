786971c08bf416cd06006a009b17c433a7775cf4
Wes Higbee <kareemabdallah@hotmail.com>
2020-02-13 01:59:00 +0100

    Adding gitignore & README

---
+++ README.md
@@ -0,0 +1,6 @@
+# Using Terraform to Manage Applications and Infrastructure
+
+This is a drill using Terraform with some tools like: Docker, Swarm, Jenkins, AWS, Azure, K8s 
+
+It contains a simple Terraform Infrastructure scripts, a k8s deployment descriptor and the command line automated build tool that the article describes the creation of a simple ci/cd application.
+
--- main.tf
+++ main.tf
@@ -14,7 +14,13 @@ resource "docker_container" "container_id" {
 }
 
 #Output the IP Address of the Container
-output {
-
-
+output "ip_address" {
+  value =  "${docker_container.container_id.ip_address}"
+  description = "The IP for the container."
 }
+#Output the Name of the Container
+output "container_name" {
+  value = "${docker_container.container_id.name}"
+  description = "The name of the container."
+}
+
